52e11e60499b8d69cf316d6b142816358d26dfb3
Miguel-Lopes-Git <lopesmiguel51100@gmail.com>
2026-05-04 17:57:05 +0200

    rename .env to .env.exemple

--- app/auth.py
+++ app/auth.py
@@ -6,12 +6,15 @@ import os
 
 load_dotenv()
 
+# Get the API key from the header
 api_key_header = api_key.APIKeyHeader(name="X-API-KEY")
 
-
 async def validate_api_key(key: str = Security(api_key_header)):
+    # Validate the API key against the environment variable
     if key != os.getenv("API_KEY"):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized - API Key is wrong"
         )
+    
+    # If the API key is valid, return None
     return None
